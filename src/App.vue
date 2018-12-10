04e03d65b4c9801ef0638b23cd8b26b4d629393e
韦华文 <fly@ms.local>
2018-12-10 17:34:19 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png">
+    <router-view/> -->
+
+    vue-cli
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style lang="less">
+@color: red;
+#app {
+  background: @color;
+}
+</style>
